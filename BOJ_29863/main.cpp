257c6e46d3e1be4f9f2169f71d1e53dcdbab12bb
protarie <protarie@gmail.com>
2023-10-06 22:05:48 +0900

    초기 커밋.

---
+++ BOJ_29863/main.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <tuple>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+using ll = long long;
+using vb = vector<bool>;
+using vvb = vector<vb>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using pii = pair<int, int>;
+using vii = vector<pii>;
+using pll = pair<ll, ll>;
+using vll = vector<pll>;
+
+int main() {
+	ios::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+
+	b += 24;
+
+	auto ans = b - a;
+	if (24 < ans) {
+		ans -= 24;
+	}
+
+	cout << ans;
+
+	return 0;
+}
